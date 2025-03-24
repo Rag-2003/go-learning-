9c6739bfa0ee2c1cc695783b3b45cae2de02b78f
Rag-2003 <btech25030.21@bitmesra.ac.in>
2025-03-24 12:19:12 +0530

    new learning day-5

--- main.go
+++ main.go
@@ -2,15 +2,35 @@ package main
 
 import (
 	"fmt"
-	
 	// "golang-learn1/myutil"
 )
 
 func main() {
-	fmt.Println("hello go lang")
+	// fmt.Println("hello go lang")
 	// myutil.PrintMessage("my name is")
 	// myutil.PrintMessage("my age")
-	var name string = "Raghav"
-	fmt.Println(name)
+	// var name string = "Raghav"
+	// fmt.Println(name)
+	var money int = 43000
+	fmt.Println(money)
+	var currency = 3400
+	fmt.Println("this is  my currency", currency)
+
+	//float example
+	var dimentions float64 = 87.34
+	fmt.Println("dimentions are", dimentions)
+
+	//boolean example
+	var decidsed bool = false //In  boolean  data type we can  change the value from  true to  false  but in  integer or any other data type we can reassign  the variable accept boolean
+	decidsed = true
+	fmt.Println("decidsed is", decidsed)
+
+	//one ore kind of variable we can defined as :
+
+	 const person = "valueis raghav"
+	 const person1 = 123
+	fmt.Println(person,person1)
+
+	
 
 }
