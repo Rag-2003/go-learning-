3e6042fd7d1fe66c65bd00618520c4661ae699de
Rag-2003 <btech25030.21@bitmesra.ac.in>
2025-04-01 16:06:12 +0530

    change in  slices

--- slices/slice.go
+++ slices/slice.go
@@ -4,12 +4,14 @@ import "fmt"
 
 func main() {
 	numbers := []int{1, 2, 3, 4, 5}
-	numbers = append(numbers, 6,7,8,9,10)
+	numbers = append(numbers, 6, 7, 8, 9, 10)
 	fmt.Println("number has data type", numbers)
 	fmt.Printf("How to check  the type %T\n", numbers)
 	fmt.Println("length:", len(numbers))
+	fmt.Println(("make iot more attractivre more then wizulazile"))
+	another()
 }
-func another(){
-   main := 123
-   fmt.Println("the number should be more modified in  this format to ",  main)
+func another() {
+	main := 123
+	fmt.Println("the number should be more modified in  this format to ", main)
 }
