e5aaafcc10dc57046b16e8a7e8bbb8f3635afb8f
Rag-2003 <btech25030.21@bitmesra.ac.in>
2025-04-04 09:54:53 +0530

    more slice

--- slices/slice.go
+++ slices/slice.go
@@ -1,17 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	numbers := []int{1, 2, 3, 4, 5}
-	numbers = append(numbers, 6, 7, 8, 9, 10)
-	fmt.Println("number has data type", numbers)
-	fmt.Printf("How to check  the type %T\n", numbers)
-	fmt.Println("length:", len(numbers))
-	fmt.Println(("make iot more attractivre more then wizulazile"))
-	another()
-}
-func another() {
-	main := 123
-	fmt.Println("the number should be more modified in  this format to ", main)
+	// numbers := [] int32{1,2}
+	// numbers = append(numbers, 6, 7, 8, 9, 10)
+	// fmt.Println("number has data type", numbers)
+	// fmt.Printf("How to check  the type %T\n", numbers)
+	// fmt.Println("length:", len(numbers))
+	// fmt.Println(("make iot more attractivre more then wizulazile"))
+	// numbers := make([]float32, 3, 5)
+	// numbers = append(numbers, 4)
+// 	numbers = append(numbers, 43)
+// 	numbers = append(numbers, 45)
+// 	numbers = append(numbers, 4)
+// 	numbers = append(numbers, 43)
+// 	numbers = append(numbers, 45)
+
+// 	fmt.Println("slice", numbers)
+// 	fmt.Println("length", len(numbers))
+// 	fmt.Println("capacity", cap(numbers))
+rag()
+ }
+
+func rag() {
+	 number1 := make([]string, 0)
+	fmt.Println("addition  nbumbr we can  ad over the string", number1)
 }
