40cd077586e015359016dd63b1a71df411648e23
Rag-2003 <btech25030.21@bitmesra.ac.in>
2025-04-07 09:39:54 +0530

    changes

---
+++ if-else/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	// x := 10
+	// if x >= 12 {
+	// 	fmt.Println("yes method is correct according to  the work flow")
+	// } else if x == 10 {
+	// 	fmt.Println("may be its work  according to  the flow")
+	// } else {
+	// 	fmt.Println("no  needed for the correction for the flow of interest ")
+	// }
+
+	// y := 23
+	// if y >=23 {
+	// 	fmt.Println("the values is correctn")
+	// } else {
+	// 	fmt.Println("the value must be equal to the flow of intrest")
+	// }
+	x := 10
+	y := 15
+	if x < 5 && y > 5 {
+		fmt.Println("y is greater then five 5")
+	} else {
+		fmt.Println("y is not in  handes")
+	}
+
+}
