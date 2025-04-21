1d09d50195686d07d737d7fceaa117a4bdc761ac
Rag-2003 <btech25030.21@bitmesra.ac.in>
2025-04-21 17:16:46 +0530

    a

--- defer/main.go
+++ defer/main.go
@@ -1,13 +1,60 @@
+// package main
+
+// import "fmt"
+
+// func early() int {
+// 	defer fmt.Println("the vlue shuld")
+// 	return 1
+
+// }
+// func value() {
+// 	for i := 0; i <= 3; i++ {
+// 		defer fmt.Println(i)
+// 	}
+// }
+// func main() {
+// 	// defer new(1)
+// 	// new(3)
+// 	// defer new(4)
+// 	// value()
+// 	// early()
+
+// }
+
+//package main
+
+// import (
+//     "log"
+//     "os"
+// )
+
+// func readFile() {
+//     file, err := os.Open("temp.txt")
+//     if err != nil {
+//         log.Fatal("Error opening file:", err)
+//     }
+//     defer file.Close()
+
+//     // Perform operations on the file
+//     log.Println("File opened successfully")
+// }
+
+// func main() {
+// 	 readFile()
+// }
+
+// return value as defer :  defer function executeafter return statement but beforethe function actully return 
 package main
 
 import "fmt"
 
-func new(i int) {
-	fmt.Println("the vlue shuld", i)
-
+func modifyReturn() (result int) {
+	defer func() {
+		result--
+	}()
+	return 0
 }
+
 func main() {
-	defer new(1)
-	new(3)
-	defer new(4)
+	fmt.Println(modifyReturn()) // Output: 1
 }
--- switchcase/main.go
+++ switchcase/main.go
@@ -1,7 +1,34 @@
 package main
 
-
+import (
+	"fmt"
+)
 
 func main() {
-//
+
+	// }
+	day := 2
+	switch day {
+	case 1:
+		fmt.Println("it's on monday")
+	case 2:
+		fmt.Println("its on tuesday")
+	default:
+		fmt.Println("df")
+	}
+
+	switch day {
+	case 1, 2, 3:
+		fmt.Println("cvdf")
+	case 4, 5:
+		fmt.Println("cvdv")
+	default:
+		fmt.Println("fSFF")
+	}
+
+	var x interface{} = 6
+	switch x.(type) {
+	case int:
+		fmt.Println("x is integer")
+	}
 }
